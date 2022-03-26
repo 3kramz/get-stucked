a5c61bbbd7d112145153fb01f8f65d639422363b
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-03-26 10:41:28 +0600

    Header aadded

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./header.css"
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Get stucked?</h1>
+            <h4>We are here for you.</h4>
+        </div>
+    );
+};
+
+export default Header;
